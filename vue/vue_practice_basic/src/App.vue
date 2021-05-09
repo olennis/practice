fa7e91b25873b985cc41d6ecc53334b61ba11067
donghun dennis kim <kisses217@gmail.com>
2021-05-09 16:14:09 +0900

    :memo: 20210509 practice vue 기본 틀 구성
    태그 수정

--- vue/vue_practice_basic/src/App.vue
+++ vue/vue_practice_basic/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
+  <div id="app">
     <h1>vue_practice</h1>
     <input type="text" />
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
 
 <style></style>
