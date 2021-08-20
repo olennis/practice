f85d0fa012bd683cdec82ec9f45f29a189180233
donghun dennis kim <kisses217@gmail.com>
2021-08-20 20:58:43 +0900

    👉 20210820 practice markup_github_practice

---
+++ vue/vue_practice_github_clone/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
+</style>
---
+++ vue/vue_practice_github_clone/src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <header>
+    <h1>
+      <img class="img_logo" alt="logo" src="../assets/logo.png" />
+    </h1>
+    <div class="wrap_search">
+      <input class="input_header" placeholder="search" />
+      <button class="btn_search">click</button>
+    </div>
+
+    <nav>
+      <ul class="list_gnb">
+        <li><a>pull request</a></li>
+        <li><a>issue</a></li>
+        <li><a>market place</a></li>
+        <li><a>explore</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<script>
+export default {};
+</script>
+<style>
+header {
+  height: 62px;
+  line-height: 30px;
+  color: #fff;
+  background-color: rgb(37, 41, 46);
+  padding: 16px 32px;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+h1 {
+  float: left;
+}
+nav {
+  overflow: hidden;
+}
+
+.img_logo {
+  width: 30px;
+  height: 30px;
+}
+.input_header {
+  vertical-align: middle;
+  margin-left: 12px;
+  min-height: 28px;
+  background: none;
+  border: 0;
+  box-shadow: none;
+  padding-bottom: 0;
+  padding-top: 0;
+  width: 240px;
+  box-sizing: border-box;
+}
+.wrap_search {
+  float: left;
+  padding: 5px 12px;
+  width: 270px;
+}
+.btn_search {
+  width: 20px;
+  height: 20px;
+  vertical-align: middle;
+  overflow: hidden;
+}
+.list_gnb {
+  overflow: hidden;
+  float: left;
+}
+.list_gnb li {
+  float: left;
+  margin-left: 10px;
+}
+</style>
