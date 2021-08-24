d988250d16c8d8be2a8f8ff70e7a2679941bd140
olennis <kisses217@gmail.com>
2021-08-24 14:48:26 +0900

    👉 20210824 practice header_animation

--- vue/vue_practice_github_clone/src/components/Header.vue
+++ vue/vue_practice_github_clone/src/components/Header.vue
@@ -5,23 +5,47 @@
         ><img class="img_logo" alt="logo" src="../assets/logo.png"
       /></a>
     </h1>
-    <div class="wrap_search">
-      <input class="input_header" placeholder="search" />
-      <button class="btn_search">/</button>
-    </div>
-
+    <transition name="input-click">
+      <div class="wrap_search" :class="{ wrap_search_active: isFocus }">
+        <input
+          class="input_header"
+          placeholder="search"
+          type="text"
+          @click="focusHandler"
+          @blur="focusHandler"
+        />
+        <button type="button" class="btn_search">/</button>
+      </div>
+    </transition>
     <nav>
       <ul class="list_gnb">
-        <li><a>pull request</a></li>
-        <li><a>issue</a></li>
-        <li><a>market place</a></li>
-        <li><a>explore</a></li>
+        <li><a class="link_gnb">pull request</a></li>
+        <li><a class="link_gnb">issue</a></li>
+        <li><a class="link_gnb">market place</a></li>
+        <li><a class="link_gnb">explore</a></li>
       </ul>
     </nav>
+    <div class="wrap_icons">
+      <button type="button" class="ico_comm">알림</button>
+      <button type="button" class="ico_comm">메뉴</button>
+      <button type="button" class="ico_comm">프로필</button>
+    </div>
   </header>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isFocus: false,
+    };
+  },
+  methods: {
+    focusHandler() {
+      this.isFocus = !this.isFocus;
+      console.log(this.isFocus);
+    },
+  },
+};
 </script>
 <style>
 header {
@@ -32,6 +56,7 @@ header {
   padding: 16px 32px;
   box-sizing: border-box;
   overflow: hidden;
+  position: relative;
 }
 h1 {
   float: left;
@@ -58,7 +83,7 @@ nav {
 .wrap_search {
   float: left;
   padding: 5px;
-  width: 270px;
+  width: 260px;
   border: 1px solid rgb(70, 77, 85);
   margin-left: 20px;
   height: 20px;
@@ -82,6 +107,39 @@ nav {
 }
 .list_gnb li {
   float: left;
+  margin-left: 20px;
+}
+.link_gnb {
+  font-weight: bold;
+}
+.wrap_icons {
+  position: absolute;
+  right: 30px;
+  top: 16px;
+}
+.ico_comm + .ico_comm {
   margin-left: 10px;
+}
+.wrap_search_active {
+  width: 300px;
+}
+
+.input-click-enter-active {
+  animation: all 0.3s;
+}
+
+.input-click-leave-active {
+  animation: all 0.8s;
+}
+@keyframes input-click {
+  0% {
+    transition: all 0.3s;
+  }
+  50% {
+    transition: all 0.3s;
+  }
+  100% {
+    transition: all 0.3s;
+  }
 }
 </style>
