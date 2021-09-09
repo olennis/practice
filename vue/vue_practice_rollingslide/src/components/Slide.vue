2390fe77335886d99c81394821af4d2ea86adf57
olennis <kisses217@gmail.com>
2021-09-09 20:58:04 +0900

    👉 20210909 practice header_markup_practice

---
+++ vue/vue_practice_rollingslide/src/components/Slide.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(slide, idx) in slides" :key="`slideIdx${idx}`">
+        <img :src="slide" v-if="visibleNum === idx" />
+      </li>
+    </ul>
+    <button @click="slidePrev">prev</button>
+    <button @click="slideNext">next</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      slides: [
+        "https://placeimg.com/300/300/any",
+        "https://placeimg.com/300/300/animals",
+        "https://placeimg.com/300/300/arch",
+        "https://placeimg.com/300/300/nature",
+        "https://placeimg.com/300/300/tech",
+      ],
+      visibleNum: 0,
+    };
+  },
+  methods: {
+    slidePrev() {
+      if (this.visibleNum === 0) return;
+      else this.visibleNum -= 1;
+    },
+    slideNext() {
+      console.log(this.visibleNum);
+      if (this.visibleNum === this.slides.length - 1) {
+        this.visibleNum = 0;
+      } else this.visibleNum += 1;
+    },
+  },
+};
+</script>
+<style>
+ul {
+  overflow: hidden;
+}
+li {
+  list-style: none;
+}
+</style>
