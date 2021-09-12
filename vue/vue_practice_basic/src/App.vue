7538201ebf9323c0f2845a796068929f97ce80e2
olennis <kisses217@gmail.com>
2021-09-13 08:14:01 +0900

    👉20210913 practice 기존코드 초기화

--- vue/vue_practice_basic/src/App.vue
+++ vue/vue_practice_basic/src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <section>
-      <ul class="list_test">
-        <li class="red"></li>
-        <li class="blue"></li>
-        <li class="green"></li>
-        <li class="yellow"></li>
-        <li class="pink"></li>
-        <li class="black"></li>
-      </ul>
-    </section>
+   
   </div>
 </template>
 
@@ -22,11 +13,7 @@ export default {
     };
   },
   components: {},
-  methods: {
-    isOpen: function() {
-      this.popup = !this.popup;
-    },
-  },
+ 
 };
 </script>
 
@@ -34,38 +21,3 @@ export default {
 #app {
   position: relative;
 }
-section {
-  max-width: 1400px;
-  margin: 0 auto;
-  background-color: #909090;
-}
-.list_test {
-  overflow: hidden;
-}
-.list_test li {
-  list-style: none;
-  float: left;
-  width: 400px;
-  height: 400px;
-  margin: 20px;
-}
-.red {
-  background-color: red;
-}
-.blue {
-  background-color: blue;
-}
-.yellow {
-  background-color: yellow;
-}
-.green {
-  background-color: green;
-}
-.black {
-  background-color: black;
-}
-
-.pink {
-  background-color: pink;
-}
-</style>
