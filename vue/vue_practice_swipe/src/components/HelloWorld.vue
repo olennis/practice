5259d7579e9edfdfa39a9160514da8dad1296cbd
olennis <kisses217@gmail.com>
2021-10-09 13:51:21 +0900

    👉20211009 practice vue-swiper 연습

---
+++ vue/vue_practice_swipe/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <swiper>
+    <swiper-slide>1</swiper-slide>
+    <swiper-slide>2</swiper-slide>
+    <swiper-slide>3</swiper-slide>
+    <swiper-slide>4</swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+export default {
+  components: {
+    swiper,
+    swiperSlide,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.d1 {
+  background: red;
+}
+.d2 {
+  background: blue;
+}
+.d3 {
+  background: pink;
+}
+.d4 {
+  background: yellow;
+}
+</style>
