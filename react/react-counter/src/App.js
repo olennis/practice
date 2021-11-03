85a69e72164c3ca1aff699ae9b5ffe3ea989fbdc
olennis <kisses217@gmail.com>
2021-11-03 18:00:18 +0900

    👉20211103 practice recoil 연습

--- react/react-counter/src/App.js
+++ react/react-counter/src/App.js
@@ -1,26 +1,13 @@
 import "./App.css";
-import { useState } from "react";
 import Counter from "./components/Counter";
+import { RecoilRoot } from "recoil";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
-      <Counter count={count}></Counter>
+      <RecoilRoot>
+        <Counter></Counter>
+      </RecoilRoot>
     </div>
   );
 }
