dfd01eef8097552f5fe3b78e9ea533ac1bf7e162
olennis <kisses217@gmail.com>
2021-11-05 15:15:10 +0900

    👉20211105 practice react-timer 연습

---
+++ react/react-stopwatch/src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [isStart, setIsStart] = useState(false);
+  const [sec, setSec] = useState(55);
+  const [min, setMin] = useState(59);
+  const [hour, setHour] = useState(0);
+  useEffect(() => {
+    if (isStart) {
+      setTimeout(() => {
+        if (isOver(sec)) {
+          if (isOver(min)) {
+            setMin(0);
+            setHour(hour + 1);
+          } else {
+            setMin(min + 1);
+          }
+          setSec(0);
+        } else {
+          setSec(sec + 1);
+        }
+      }, 1000);
+    }
+  }, [sec, isStart]);
+  const isOver = (time) => {
+    return time > 58;
+  };
+  const startHandler = () => {
+    setIsStart(true);
+  };
+  const stopHandler = () => {
+    setIsStart(false);
+  };
+
+  return (
+    <div className="App">
+      <div className="stopwatch">
+        {hour < 10 ? <span>0{hour}</span> : <span>{hour}</span>}
+        <span> : </span>
+        {min < 10 ? <span>0{min}</span> : <span>{min}</span>}
+        <span> : </span>
+        {sec < 10 ? <span>0{sec}</span> : <span>{sec}</span>}
+      </div>
+      <button onClick={startHandler}>start</button>
+      <button onClick={stopHandler}>stop</button>
+    </div>
+  );
+}
+
+export default App;
