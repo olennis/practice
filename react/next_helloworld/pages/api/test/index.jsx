3bccb0d9ef58ed27361fcadf6a2b9374ea09e539
olennis <kisses217@gmail.com>
2021-11-20 15:34:39 +0900

    👉20211120 practice nextjs basic

--- react/next_helloworld/pages/api/test/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function index() {
-  return <div>next test중임당</div>;
-}
-
-export default index;
---
+++ react/next_helloworld/pages/test/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+function index() {
+  return (
+    <div>
+      <Link href="/test/test-depth2">hello world</Link>
+    </div>
+  );
+}
+
+export default index;
