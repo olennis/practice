321945b889a4628f22da243fe8464cd61f7ca606
olennis <kisses217@gmail.com>
2021-12-11 15:11:55 +0900

    👉20211211 practice react login/인풋 가져오기

--- react/next/nextjs-practice/pages/login/index.tsx
+++ react/next/nextjs-practice/pages/login/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
-export default function index() {
-    const loginHandler = () =>{
+type LoginInfo =  {
+    id : string,
+    pw : string
+};
+export default function Index() {
+    const [inputObj, setInputObj] = useState<LoginInfo>({
+        id : '',
+        pw : ''
+    })
+    const loginInputHandler = (e) => {
+        setInputObj({...inputObj,[e.target.name] : e.target.value})
+    }
+    const loginBtnHandler = () =>{
+        console.log(inputObj)
     }
     return (
         <div>
             <div>
                 <label htmlFor='idInput'>id</label>
-                <input type='text' id='idInput'></input>
+                <input type='text' id='idInput' name='id' onChange={loginInputHandler}></input>
             </div>
             <div>
                 <label htmlFor='pwInput'>pw</label>
-                <input type='password' id='pwInput'></input>
+                <input type='password' id='pwInput' name='pw' onChange={loginInputHandler}></input>
             </div>
-            <button type='button' onClick={loginHandler}>login</button>
+            <button type='button' onClick={loginBtnHandler}>login</button>
         </div>
     )
 }
