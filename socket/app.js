453ca762c33ef82874d3f2df6ffaa3f88349d91b
donghun dennis kim <kisses217@gmail.com>
2022-02-01 18:05:38 +0900

    👨🏻‍💻 feat: socketjs basic - user connected

--- socket/app.js
+++ socket/app.js
@@ -1,9 +1,21 @@
-const io = require("socket.io");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-io.socket.on("connection", function (socket) {
-  socket.emit("news", { hello: "world" });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-  socket.on("my other event", function (data) {
-    console.log(data);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
