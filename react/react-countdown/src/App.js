d31038f1a1c4a6632b054c0508e90b8911765b77
donghun dennis kim <kisses217@gmail.com>
2022-02-08 20:12:24 +0900

    👨🏻‍💻 test: timer practice

--- react/react-countdown/src/App.js
+++ react/react-countdown/src/App.js
@@ -3,37 +3,28 @@ import { useEffect, useState } from "react";
 function App() {
   const [min, setMin] = useState(1);
   const [sec, setSec] = useState(0);
-  const [mSec, setMsec] = useState(0);
-
   useEffect(() => {
     const countdown = setInterval(() => {
-      if (parseInt(mSec) > 0) {
-        setMsec(parseInt(mSec) - 1);
-      }
-      if (parseInt(mSec) === 0) {
-        if (parseInt(min) === 0 && parseInt(sec) === 0) {
+      if (parseInt(sec) > 0) {
+        setSec(parseInt(sec) - 1);
+      } else {
+        if (parseInt(min) === 0) {
           //완료 됐을 때
           console.log("finish!!");
           clearInterval(countdown);
         } else {
-          if (parseInt(sec) > 0) {
-            setSec(parseInt(sec) - 1);
-          } else {
-            setSec(59);
-            setMin(parseInt(min) - 1);
-          }
-          setMsec(99);
+          setSec(59);
+          setMin(parseInt(min) - 1);
         }
       }
-    }, 10);
+    }, 1000);
     return () => clearInterval(countdown);
-  }, [mSec]);
+  }, [sec]);
 
   return (
     <div className="App">
       <div>
-        {min < 10 ? `0${min}` : min}:{sec < 10 ? `0${sec}` : sec}:
-        {mSec < 10 ? `0${mSec}` : mSec}
+        {min < 10 ? `0${min}` : min}:{sec < 10 ? `0${sec}` : sec}
       </div>
     </div>
   );
